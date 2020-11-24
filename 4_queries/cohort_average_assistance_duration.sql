215fd1cf278b91fdb687da4c00924f2be292fc47
Rahul <rahul.shial@gmail.com>
2020-11-24 16:51:59 +0000

    4_queries Average Total Cohort Assistance Duration

---
+++ 4_queries/cohort_average_assistance_duration.sql
@@ -0,0 +1,10 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM (SELECT cohorts.name AS name,
+  SUM(completed_at - started_at) AS total_duration
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+JOIN assistance_requests ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY total_duration
+) as total_durations;
+
