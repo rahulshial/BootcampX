9fbea392ff2c900dea59d6a597c059e0397d1892
Rahul <rahul.shial@gmail.com>
2020-11-24 17:07:49 +0000

    4_queries Name of Teachers assisting Cohorts

---
+++ 4_queries/name_teachers_assisted_cohorts.sql
@@ -0,0 +1,8 @@
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teacherS ON teachers.id = teacher_id
+JOIN students on students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
