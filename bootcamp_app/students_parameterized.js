704092a5105545fea976b77a215ee1965ff6763c
Rahul <rahul.shial@gmail.com>
2020-11-25 22:50:04 +0000

    Student & Teachers Parameterized queries

---
+++ bootcamp_app/students_parameterized.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+const queryString = `
+SELECT s.id, s.name, cohorts.name as cohort
+FROM students s
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
